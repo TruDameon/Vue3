5a95a2a1e0855786bb729901036b6074e814b0fb
Dameon <shubhamsingh0602@gmail.com>
2021-12-29 01:45:25 +0530

    Day1 Sec2:14

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-Vue.createApp({}).mount('#app')
+// var lastName1 = 'Doe'
+
+var vm = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe'
+        }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName.toUpperCase()}`
+        }
+    }
+}).mount('#app');
+
+setTimeout(() => {
+    vm.firstName = 'Bob' // Ideally we should use vm.$data.firstName but we are not doing that here because of a property of Vue called proxy. Vue has proxy getters and setters to manipulate data directly. This can be seen directly by entering 'vm' in the console and seing it's values.
+}, 2000);
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             firstName: 'Jane',
+//             lastName: 'Doe'
+//         }
+//     }
+// }).mount('.app2')
